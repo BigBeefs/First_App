5a350595c62bd89ea8cb86e29d531aebd4f6957a
BigBeefs <bigbeefs98@gmail.com>
2023-03-24 12:39:46 +0100

    test streamlit

---
+++ app.py
@@ -0,0 +1,50 @@
+import streamlit as st  # Con il comando streamlit run app.py crea un server front end
+
+st.text('Prova testo')
+
+
+import pandas as pd
+import numpy as np
+
+
+
+age = st.slider('How old are you?', 0, 130, 2)
+
+age1 = st.slider('How old are you??', 0, 130, 3)
+
+
+
+number = st.number_input('Insert a number',0)
+
+d1={'a':[age,age1,number]}
+
+
+chart_data = pd.DataFrame(d1)
+
+st.area_chart(chart_data)
+
+
+import time
+
+
+
+# progress_text = "Operation in progress. Please wait."
+# my_bar = st.progress(0, text=progress_text)
+
+# for percent_complete in range(100):
+
+#     if percent_complete < 99:
+#         time.sleep(0.1)
+#         my_bar.progress(percent_complete + 1, text=progress_text)
+#     else :
+#         my_bar.progress(percent_complete + 1, text='Complete')
+        
+df = pd.read_csv('company_sales_data.csv')
+
+
+
+chart_data1 = pd.DataFrame(df)
+
+st.line_chart(chart_data1)
+
+
